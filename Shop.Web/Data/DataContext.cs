a889f634295344e547c44570f391fb5f2e834080
Orlando9510 <orlandoespinosa166321@correo.itm.edu.co>
2019-02-21 18:57:23 -0500

    Add products controller

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace Shop.Web.Data
+{
+    // diseño total nuevo del entity framework
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        // Adicionar tabla products a la base de datos por medio de una propiedad
+        public DbSet<Product> Products { get; set; }
+        
+        // para conectarse a la base de datos
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+               
+        }
+
+
+    }
+}
---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Shop.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        // Clave primaria
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // Darle formato a la columna Price. Que muestre la moneda que esta configurada el sistema
+        // currency de 2 - (0:2) separador demiles con 2 decimales
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        // Display Sirve para colocarle nombre para el usuario
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Availabe?")]
+        public bool IsAvailabe { get; set; }
+
+        //Numerico de dos decimales
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+
+}
