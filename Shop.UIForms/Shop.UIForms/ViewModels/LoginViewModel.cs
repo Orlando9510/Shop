2049f825c3184b3ee6a27e11da9b9786bc5c47f1
Orlando9510 <orlandoespinosa166321@correo.itm.edu.co>
2019-02-25 18:18:46 -0500

    Login View Model

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,7 +1,62 @@
 ﻿
+
 namespace Shop.UIForms.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
     public class LoginViewModel
     {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "orlandoespinosa166321@correo.itm.edu.co";
+            this.Password = "123456";
+        }
+
+        private async void Login()
+        {
+            // validación de campo Email, si hay error saca mensajes de error
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            }
+
+            // validación de campo de password, si hay error saca mensajes de error
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a ´Password.",
+                    "Accept");
+                return;
+            }
+
+            if (!this.Email.Equals("orlandoespinosa166321@correo.itm.edu.co") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or password wrong.",
+                    "Accept");
+                return;
+            }
+
+            // Mensaje satisfactorio
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah!!!",
+                    "Accept");
+        }
     }
 }
